344df4ce6f131ba0e9bc097fa7c754ac8c558115
Maciej Stosio <maciekstosio@users.noreply.github.com>
2025-02-10 18:06:27 +0100

    chore: rename folders

--- scripts/build-radon-artifacts.sh
+++ scripts/build-radon-artifacts.sh
@@ -11,13 +11,13 @@ mkdir -p $out
 echo "Building webui"
 
 npm run web:export 
-mkdir -p $out/webui
-mv -f dist/_expo/static/css/* $out/webui/ # Copy newly generated files
-mv -f dist/_expo/static/js/web/* $out/webui/ # Copy newly generated files
+mkdir -p $out/redux-devtools
+mv -f dist/_expo/static/css/* $out/redux-devtools/ # Copy newly generated files
+mv -f dist/_expo/static/js/web/* $out/redux-devtools/ # Copy newly generated files
 
 echo "Building plugin"
 
 ./node_modules/.bin/esbuild src/index.ts --bundle --platform=node --outfile=redux-devtools-expo-dev-plugin.js
 
-mkdir -p $out/external
-mv -f ./redux-devtools-expo-dev-plugin.js $out/external/redux-devtools-expo-dev-plugin.js
+mv -f ./redux-devtools-expo-dev-plugin.js $out/redux-devtools-expo-dev-plugin.js
+
